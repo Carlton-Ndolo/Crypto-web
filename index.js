c4ccd585901c0c201290842be60dceb33aaf88bb
Carlton-Ndolo <carltonmutua80@gmail.com>
2024-04-10 12:22:37 +0300

    create variables to retrieve HTML data

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const baseURL = "https://api.coinlore.net/api/tickers/";
+    const cryptoDetailsContainer = document.getElementById("cryptoDetails");
+    const SearchInputContent = document.getElementById("searchInput");
+    const searchButton = document.getElementById("searchBtn");
+    const coinSelect1 = document.getElementById("coinSelect1");
+    const coinSelect2 = document.getElementById("coinSelect2");
+    const compareButton = document.getElementById("compareBtn");
+    const compareResults = document.getElementById("comparisonResult");
     
+
 })
